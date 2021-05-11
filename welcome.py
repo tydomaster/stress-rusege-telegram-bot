e8eef6354cd75c175dc33629fdf78ee10e7f30ec
tydomaster <52607612+tydomaster@users.noreply.github.com>
2021-05-11 18:02:49 +0300

    modify welcome.py

--- welcome.py
+++ welcome.py
@@ -1,15 +1,48 @@
 import os
+from os import path
 p = os.path.abspath('')
 p1 = os.path.abspath('')
-p += r'\\dbs\\'
-p1 += r'\\'
+pl = os.path.abspath('')
 
-open(p + "db_main_info.txt", "w", encoding="utf8").close()
-open(p + "db_main_info_backup.txt", "w", encoding="utf8").close()
-open(p + "db_used_info.txt", "w", encoding="utf8").close()
-open(p + "db_used_info_backup.txt", "w", encoding="utf8").close()
-open(p + "db_achievements_info.txt", "w", encoding="utf8").close()
-open(p + "db_achievements_info_backup.txt", "w", encoding="utf8").close()
-open(p1 + ".env", "w", encoding="utf8").close()
+p += r'\dbs\''
+p1 += r'\''
+pl += r'\logs\''
+
+p = p[:-1]
+p1 = p1[:-1]
+pl = pl[:-1]
+
+if path.exists("dbs") == 0:
+    os.mkdir("dbs")
+if path.exists("logs") == 0:
+    os.mkdir("logs")
+pmain = p + "db_main_info.txt"
+pmainb = p + "db_main_info_backup.txt"
+pused = p + "db_used_info.txt"
+pusedb = p + "db_used_info_backup.txt"
+pach = p + "db_achievements_info.txt"
+pachb = p + "db_achievements_info_backup.txt"
+
+if path.exists(pmain) == 0:
+    open(pmain, "w", encoding="utf8").close()
+if path.exists(pmainb) == 0:
+    open(pmainb, "w", encoding="utf8").close()
+if path.exists(pused) == 0:
+    open(pused, "w", encoding="utf8").close()
+if path.exists(pusedb) == 0:
+    open(pusedb, "w", encoding="utf8").close()
+if path.exists(pach) == 0:
+    open(pach, "w", encoding="utf8").close()
+if path.exists(pachb) == 0:
+    open(pachb, "w", encoding="utf8").close()
+if path.exists(".env") == 0:
+    f = open(p1 + ".env", "w", encoding="utf8")
+    f.write('BOT_TOKEN=\nMAIN_INFO='
+            + pmain + '\nMAIN_INFO_BACKUP='
+            + pmainb + '\nUSED_INFO='
+            + pused + '\nUSED_INFO_BACKUP='
+            + pusedb + '\nACHIEVEMENTS_INFO='
+            + pach + '\nACHIEVEMENTS_INFO_BACKUP='
+            + pachb + '\nLOGS_PATH=' + pl)
 
 print('done!')
